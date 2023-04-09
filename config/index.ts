360f85f3afdf5a122ecc9d4193df1f2f722eaf53
Mubashir Hassan <official.mhm13@gmail.com>
2023-04-10 04:23:18 +0500

    refactor: cleanup

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,7 @@
 export class Config {
-  public static readonly APP_NAME = "Wagmi - mhm13dev";
+  public static readonly APP_NAME = "Wagmi - WalletConnect - mhm13dev";
 
   public static readonly Blockchain = {
-    INFURA_API_KEY: process.env.NEXT_PUBLIC_INFURA_API_KEY!,
     WALLET_CONNECT_PROJECT_ID:
       process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!,
   };
